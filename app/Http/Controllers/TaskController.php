dd852ada1f69a060cc9ed18ca49e19cac223a67f
shimada <tomoya.0915.st@gmail.com>
2021-09-30 15:09:01 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Folder;
+use App\Models\Task;
+use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index(Folder $folder)
+    {
+        $folders = Auth::user()->folders()->get();
+
+        $tasks = $folder->tasks()->get();
+
+        return view('tasks.index')
+            ->with([
+                'folders' => $folders,
+                'current_folder_id' => $folder->id,
+                'tasks' => $tasks,
+            ]);
+    }
+
+    public function showCreateForm(Folder $folder)
+    {
+        return view('tasks/create')
+            ->with(['folder_id' => $folder->id]);
+    }
+
+    public function create(Folder $folder, CreateTask $request)
+    {
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $folder->tasks()->save($task);
+
+        return redirect()
+            ->route('tasks.index', ['id' => $folder->id]);
+    }
+
+    public function showEditForm(Folder $folder, Task $task)
+    {
+        $this->checkRelation($folder, $task);
+
+        return view('tasks/edit')
+            ->with(['task' => $task]);
+    }
+
+    public function edit(Folder $folder, Task $task, EditTask $request)
+    {
+        $this->checkRelation($folder, $task);
+
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
+
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+        }
+    }
+}
---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Requests\CreateTask;
+use Carbon\Carbon;
+
+class TaskTest extends TestCase
+{
+     // テストケースごとにデータベースをリフレッシュしてマイグレーションを再実行する
+     use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed('FoldersTableSeeder');
+    }
+
+    public function test_due_date_should_be_date()
+    {
+        $response = $this->post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => 123, // 不正なデータ（数値）
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日 には日付を入力してください。',
+        ]);
+    }
+    public function test_due_date_should_not_be_past()
+    {
+        $response = $this->post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::yesterday()->format('Y/m/d'), // 不正なデータ（昨日の日付）
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日 には今日以降の日付を入力してください。',
+        ]);
+    }
+
+    public function test_status_should_be_within_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
+
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+        ]);
+    }
+}
